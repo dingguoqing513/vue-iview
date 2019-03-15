70f544580fac482071a47620702f9f2fef916686
dingguoqing <yamuyy139@126.com>
2019-03-15 09:20:47 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="T-container">
+    <section>
+      Hello Dingguoqing
+    </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'BaseEnter',
+  data () {
+    return {
+      
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
