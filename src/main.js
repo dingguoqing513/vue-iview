f987520709459f29fa3f8ea4e707783addb0eab9
dingguoqing <yamuyy139@126.com>
2019-03-15 18:21:49 +0800

    add menu

--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-// The Vue build version to load with the `import` command
-// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
+import iView from 'iview' // 引入iView
+import 'iview/dist/styles/iview.css' // 引入iView css
 import router from './router'
+import '../static/main.css' // 引入公共样式
 
 Vue.config.productionTip = false
-
-/* eslint-disable no-new */
+Vue.use(iView)
 new Vue({
   el: '#app',
   router,
